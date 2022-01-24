ad358cee771285d4f7bdedba5b4853dc65535f63
mooncero <97421668+mooncero@users.noreply.github.com>
2022-01-24 17:03:50 +0700

    Create banner_300.js

---
+++ banner_300.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '6601a92247b0ac5fd9bab6d79ff7a5b9',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://writtenanonymousgum.com/6601a92247b0ac5fd9bab6d79ff7a5b9/invoke.js"></scr' + 'ipt>');
