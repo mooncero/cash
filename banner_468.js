faffcc6bb74ab2b06f584e5456d4197488ea8200
mooncero <97421668+mooncero@users.noreply.github.com>
2022-01-24 17:06:43 +0700

    Create banner_468.js

---
+++ banner_468.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '3fce88ad90c55b408ceec7eef8d954ab',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://writtenanonymousgum.com/3fce88ad90c55b408ceec7eef8d954ab/invoke.js"></scr' + 'ipt>');
