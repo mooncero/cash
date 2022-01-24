70888205705e2a2340c56c539c163eddf13d954c
mooncero <97421668+mooncero@users.noreply.github.com>
2022-01-24 17:07:23 +0700

    Create banner_728.js

---
+++ banner_728.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'cf0cf5a5f2911c38dc4a02f8b02ad8e2',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://writtenanonymousgum.com/cf0cf5a5f2911c38dc4a02f8b02ad8e2/invoke.js"></scr' + 'ipt>');
